494d219734c32f8b035059d8a910694a4f8d22ac
Sruti Samantaray <134088431+srutisam@users.noreply.github.com>
2025-04-15 20:19:44 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from CI/CD app!');
+});
+
+module.exports = app;
+
+if (require.main === module) {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
